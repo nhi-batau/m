73e1211219b00bc1be10c1a9613bb1610b76e96b
Abhisehkkiar15 <mpsystem05@gmail.com>
2025-04-10 15:14:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,9 +6,9 @@ import os
 from pymongo import MongoClient
 
 class Config(object):
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7221760137:AAGvD4ICBuaFufzjsl2woOlHOotxQLlM5xU")
-    API_ID = int(os.environ["API_ID", 15958423]
-    API_HASH = os.environ["API_HASH", "0f38f0c37cec744bccb074b5180e37b0"]
-    AUTH_USERS = "5128979564"
-    MONGO_URI = os.environ.get("MONGO_DB_URI", "your_mongo_db_uri")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7795464807:AAF54VE2BT28DlIN3v0pebk3SXFD1l5tYDY")
+    API_ID = int(os.environ["API_ID", 24037760]
+    API_HASH = os.environ["API_HASH", "dccc3070f1c12ab155011f14c3d6ae6a"]
+    AUTH_USERS = "7834875502"
+    MONGO_URI = os.environ.get("MONGO_DB_URI", "mongodb+srv://mpsystem05:ZzYGIZ0a4PCUQkKm@cluster0.eppygqi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     
